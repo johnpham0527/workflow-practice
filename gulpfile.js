9e91ad67a85077f76232479bd466b7396b9d5e29
John Pham <mr.john.pham@gmail.com>
2020-05-16 06:19:06 -0400

    Require gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const {src, dest} = require('gulp');
+const babel = require('gulp-babel');
 
 function html(cb) {
   src('src/calculator.html').pipe(dest('build'));
   cb();
 }
+
+
 
 exports.default = html
